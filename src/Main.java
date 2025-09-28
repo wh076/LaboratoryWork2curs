b99f3990924368d54b92edc8b5fcda030c285c70
wh076 <mt1337228@gmail.com>
2025-09-28 13:37:16 +0700

    feat: add Main class

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        Balance balance = new Balance();
+        Scanner scanner = new Scanner(System.in);
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("What is your name?\n> ");
-        String name = in.nextLine();
+        System.out.println("левая чаша:");
+        int left = scanner.nextInt();
+        balance.addLeft(left);
 
-        System.out.printf("Hello, %s!", name);
-        in.close();
+        System.out.println("правая чаша:");
+        int right = scanner.nextInt();
+        balance.addRight(right);
 
+        String result = balance.result();
+        System.out.println(result);
 
+        scanner.close();
     }
 }
