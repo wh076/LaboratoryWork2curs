f071ae7285bb7ba20ebbb7443e7ba217f2b78470
wh076 <mt1337228@gmail.com>
2025-09-28 19:26:55 +0700

    feat: add Main class

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
+        Table table = new Table(3, 3);
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("What is your name?\n> ");
-        String name = in.nextLine();
+        table.setValue(0, 0, 1);
+        table.setValue(0, 1, 2);
+        table.setValue(0, 2, 3);
+        table.setValue(1, 0, 4);
+        table.setValue(1, 1, 5);
+        table.setValue(1, 2, 6);
+        table.setValue(2, 0, 7);
+        table.setValue(2, 1, 8);
+        table.setValue(2, 2, 9);
 
-        System.out.printf("Hello, %s!", name);
-        in.close();
+        // Выводим таблицу
+        System.out.println("таблица:");
+        System.out.println(table.toString());
 
+        // Получаем значение
+        System.out.println("значение в [1,1]: " + table.getValue(1, 1));
 
+        // Размеры таблицы
+        System.out.println("строк: " + table.rows());
+        System.out.println("столбцов: " + table.cols());
+
+        // Среднее значение
+        System.out.println("среднее: " + table.average());
     }
 }
