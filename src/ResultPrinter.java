d010f3109108b8de6c57d34234d601b31ee8595f
wh076 <mt1337228@gmail.com>
2025-10-04 21:33:13 +0700

    feat: add ResultPrinter and WorldCounter classes

---
+++ src/ResultPrinter.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.List;
+
+public class ResultPrinter {
+
+    /**
+     * Показать заголовок и исходный текст
+     * @param counter счетчик слов
+     */
+    public void showHeader(WordCounter counter) {
+        System.out.println(counter.getText());
+        System.out.println();
+        System.out.println(" ");
+        System.out.println();
+    }
+
+    /**
+     * Показать все слова и их количество
+     * @param counter счетчик слов
+     */
+    public void showWords(WordCounter counter) {
+        System.out.println("СЛОВА И ИХ КОЛИЧЕСТВО:");
+
+
+        HashMap<String, Integer> allWords = counter.getAllWords();
+
+        for (HashMap.Entry<String, Integer> entry : allWords.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + " раз");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Показать статистику
+     * @param counter счетчик слов
+     */
+    public void showStats(WordCounter counter) {
+        System.out.println("СТАТИСТИКА:");
+
+        System.out.println("Всего разных слов: " + counter.getDifferentWordsCount());
+        System.out.println("Самое частое слово: " + counter.getMostFrequentWord());
+        System.out.println();
+    }
+
+    /**
+     * Показать отсортированный список
+     * @param counter счетчик слов
+     */
+    public void showSorted(WordCounter counter) {
+        System.out.println("ОТСОРТИРОВАННЫЙ СПИСОК (от частых к редким):");
+
+
+        List<HashMap.Entry<String, Integer>> sorted = counter.getSortedWords();
+
+        int number = 1;
+        for (HashMap.Entry<String, Integer> entry : sorted) {
+            System.out.println(number + ". " + entry.getKey() + " - " + entry.getValue() + " раз");
+            number++;
+        }
+    }
+
+    /**
+     * Показать все результаты
+     * @param counter счетчик слов
+     */
+    public void showAll(WordCounter counter) {
+        showHeader(counter);
+        showWords(counter);
+        showStats(counter);
+        showSorted(counter);
+    }
+}
