790048fad5af745a52a0e40c007ba06e09d5de9a
wh076 <mt1337228@gmail.com>
2025-11-22 14:46:14 +0700

    feat: add books.json and music-create.sql

---
+++ music-create.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS music (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
+);
+
+INSERT INTO music (title) VALUES
+('Bohemian Rhapsody'),
+('Stairway to Heaven'),
+('Imagine'),
+('Sweet Child O Mine'),
+('Hey Jude'),
+('Hotel California'),
+('Billie Jean'),
+('Wonderwall'),
+('Smells Like Teen Spirit'),
+('Let It Be'),
+('I Want It All'),
+('November Rain'),
+('Losing My Religion'),
+('One'),
+('With or Without You'),
+('Sweet Caroline'),
+('Yesterday'),
+('Dont Stop Believin'),
+('Crazy Train'),
+('Always');
